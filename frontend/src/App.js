fa5806ac8c11942f8291d7079dcbadbe7cacf351
kmjang <kyumin.jang@gmail.com>
2023-02-14 16:53:06 +0900

    질문 상세 처리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import axios from "axios";
 
 function App() {
-    const [questionList, setQuestionList] = useState([])
+  const [questionList, setQuestionList] = useState([]);
 
-    useEffect(() => {
-        axios.get('/question/list')
-        .then(response => {
-            setQuestionList(response.data)
-        })
-        .catch(error => console.log(error))
-    }, []);
+  useEffect(() => {
+    axios
+      .get("/question/list")
+      .then((response) => {
+        setQuestionList(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>제목</th>
-                        <th>작성일시</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {questionList && questionList.map((question, index) => (
-                        <tr key={question.id}>
-                            <td>{question.subject}</td>
-                            <td>{question.content}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>제목</th>
+            <th>작성일시</th>
+          </tr>
+        </thead>
+        <tbody>
+          {questionList &&
+            questionList.map((question, index) => (
+              <tr key={question.id}>
+                <td>
+                  <a href={"/question/detail/" + question.id}>
+                    {question.subject}
+                  </a>
+                </td>
+                <td>{question.createDate}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
--- src/main/java/com/mysite/main/question/QuestionController.java
+++ src/main/java/com/mysite/main/question/QuestionController.java
@@ -2,18 +2,26 @@ package com.mysite.main.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @RestController
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public List<Question> list() {
         return this.questionService.getList();
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public Question detail(@PathVariable("id") Integer id) {
+        return this.questionService.getQuestion(id);
     }
 }

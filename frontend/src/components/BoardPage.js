383304afe90112f13447a7eeefd317f229109cf1
kmjang <kyumin.jang@gmail.com>
2023-03-02 22:55:09 +0900

    페이징 1차 처리

--- frontend/src/components/BoardPage.js
+++ frontend/src/components/BoardPage.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Container, Table, Button } from "react-bootstrap";
+import { Container, Table, Button, Pagination } from "react-bootstrap";
 
 function BoardPage() {
-  const [questionList, setQuestionList] = useState([]);
+  const [paging, setPaging] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -12,7 +12,7 @@ function BoardPage() {
       .get("/question/list?page=1")
       .then((response) => {
         console.log(response.data);
-        setQuestionList(response.data.content);
+        setPaging(response.data);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -28,8 +28,8 @@ function BoardPage() {
           </tr>
         </thead>
         <tbody>
-          {questionList &&
-            questionList.map((question, index) => (
+          {paging.content &&
+            paging.content.map((question, index) => (
               <tr key={question.id}>
                 <td>{index}</td>
                 <td>
@@ -49,6 +49,52 @@ function BoardPage() {
             ))}
         </tbody>
       </Table>
+
+      {!paging.empty && (
+        <div>
+          <Pagination className="justify-content-center">
+            {paging.content &&
+              paging.content.map((question, index) => (
+                <Pagination.Item key={index} active={index === 2}>
+                  {index}
+                </Pagination.Item>
+              ))}
+          </Pagination>
+        </div>
+      )}
+
+      {/* <div th:if="${!paging.isEmpty()}">
+        <ul class="pagination justify-content-center">
+          <li
+            class="page-item"
+            th:classappend="${!paging.hasPrevious} ? 'disabled'"
+          >
+            <a class="page-link" th:href="@{|?page=${paging.number-1}|}">
+              <span>이전</span>
+            </a>
+          </li>
+          <li
+            th:each="page: ${#numbers.sequence(0, paging.totalPages-1)}"
+            th:classappend="${page == paging.number} ? 'active'"
+            class="page-item"
+          >
+            <a
+              th:text="${page}"
+              class="page-link"
+              th:href="@{|?page=${page}|}"
+            ></a>
+          </li>
+          <li
+            class="page-item"
+            th:classappend="${!paging.hasNext} ? 'disabled'"
+          >
+            <a class="page-link" th:href="@{|?page=${paging.number+1}|}">
+              <span>다음</span>
+            </a>
+          </li>
+        </ul>
+      </div> */}
+
       <Button
         variant="primary"
         style={{ marginTop: 10 }}
